26c63fd98d5422f276ba965c5d99da902c16da0d
andysseus <anorgan@gmail.com>
2022-07-26 14:18:08 -0400

    added ... passing to filter

--- R/get-matches.R
+++ R/get-matches.R
@@ -79,13 +79,16 @@ get_soccer_data <- function(data_name) {
 #' This function relies on an internal function, `uss_make_matches()`, to parse
 #' the source data.
 #'
+#'
 #' @inherit uss_make_matches params return
+#' @inheritParams dplyr::filter
 #'
 #' @examples
 #' uss_get_matches("england")
+#' uss_get_matches("england", season == 1966)
 #' @export
 #'
-uss_get_matches <- function(country = uss_countries()) {
+uss_get_matches <- function(country = uss_countries(), ...) {
 
   # 2.2.1 side effects (errors)
   #
@@ -109,7 +112,8 @@ uss_get_matches <- function(country = uss_countries()) {
   # (but we aren't doing that here)
   # https://design.tidyverse.org/dots-prefix.html
 
-  uss_make_matches(data, country)
+  uss_make_matches(data, country) |>
+    dplyr::filter(...)
 }
 
 
